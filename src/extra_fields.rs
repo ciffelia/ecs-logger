8ec5bb28ffbc28fba043f4d754627c99b422475b
Ciffelia <mc.prince.0203@gmail.com>
2023-03-31 01:06:50 +0900

    test: implement unit tests for `extra_fields`

--- src/extra_fields.rs
+++ src/extra_fields.rs
@@ -58,3 +58,131 @@ fn extend_json_map(a: &mut JsonMap, b: &JsonMap) {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use serde_json::json;
+    use std::collections::BTreeMap;
+
+    #[test]
+    fn test_set_extra_fields_ok() {
+        set_extra_fields(json!({
+            "a": 1,
+            "b": {
+                "c": 2,
+            },
+        }))
+        .unwrap();
+
+        let r = EXTRA_FIELDS.read().unwrap();
+        assert!(r.is_some());
+        assert_eq!(
+            serde_json::to_string(r.as_ref().unwrap()).unwrap(),
+            json!({
+                "a": 1,
+                "b": {
+                    "c": 2,
+                },
+            })
+            .to_string()
+        );
+    }
+
+    #[test]
+    fn test_set_extra_fields_err() {
+        let mut map = BTreeMap::new();
+        map.insert(vec![32, 64], "x86");
+        assert!(matches!(
+            set_extra_fields(map),
+            Err(SetExtraFieldsError::InvalidJson(_))
+        ));
+
+        assert!(matches!(
+            set_extra_fields(1),
+            Err(SetExtraFieldsError::NotObject)
+        ));
+    }
+
+    #[test]
+    fn test_clear_extra_fields() {
+        set_extra_fields(json!({
+            "a": 1,
+            "b": {
+                "c": 2,
+            },
+        }))
+        .unwrap();
+
+        clear_extra_fields();
+
+        let r = EXTRA_FIELDS.read().unwrap();
+        assert!(r.is_none());
+    }
+
+    #[test]
+    fn test_merge_extra_fields() {
+        set_extra_fields(json!({
+            "b": {
+                "d": 3,
+            },
+            "e": 4,
+        }))
+        .unwrap();
+
+        let mut a = json!({
+            "a": 1,
+            "b": {
+                "c": 2,
+            },
+        });
+        let a_with_extra_fields = merge_extra_fields(a.as_object_mut().unwrap().clone());
+
+        assert_eq!(
+            serde_json::to_string(&Value::Object(a_with_extra_fields)).unwrap(),
+            json!({
+                "a": 1,
+                "b": {
+                    "c": 2,
+                    "d": 3,
+                },
+                "e": 4,
+            })
+            .to_string()
+        );
+    }
+
+    #[test]
+    fn test_extend_json_map() {
+        let mut a = json!({
+            "a": 1,
+            "b": {
+                "c": 2,
+            },
+        });
+        let b = json!({
+            "b": {
+                "d": 3,
+            },
+            "e": 4,
+        });
+
+        let a = a.as_object_mut().unwrap();
+        let b = b.as_object().unwrap();
+
+        extend_json_map(a, b);
+
+        assert_eq!(
+            serde_json::to_string(a).unwrap(),
+            json!({
+                "a": 1,
+                "b": {
+                    "c": 2,
+                    "d": 3,
+                },
+                "e": 4,
+            })
+            .to_string()
+        );
+    }
+}
